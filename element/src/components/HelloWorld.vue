c988ea8f50ba7d05d753bfa84c15b61217957555
wyunfei <583514368@qq.com>
2018-01-24 11:33:26 +0800

    局部引入Button

--- element/src/components/HelloWorld.vue
+++ element/src/components/HelloWorld.vue
@@ -7,43 +7,6 @@
       <el-button type="warning">警告按钮</el-button>
       <el-button type="danger">危险按钮</el-button>
     </div>
-    <div class="block">
-      <span class="demonstration">默认不区分颜色{{ value1 }}</span>
-      <el-rate v-model="value1"></el-rate>
-    </div>
-    <div class="block">
-      <span class="demonstration">区分颜色</span>
-      <el-rate
-        v-model="value2"
-        :colors="['#99A9BF', '#F7BA2A', '#FF9900']">
-      </el-rate>
-    </div>
-    <el-pagination
-      background
-      layout="prev, pager, next"
-      :total="1000">
-    </el-pagination>
-    <template>
-      <el-table
-        v-loading="loading"
-        :data="tableData"
-        style="width: 100%">
-        <el-table-column
-          prop="date"
-          label="日期"
-          width="180">
-        </el-table-column>
-        <el-table-column
-          prop="name"
-          label="姓名"
-          width="180">
-        </el-table-column>
-        <el-table-column
-          prop="address"
-          label="地址">
-        </el-table-column>
-      </el-table>
-    </template>
   </div>
 </template>
 
--- element/src/main.js
+++ element/src/main.js
@@ -3,10 +3,10 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import ElementUI from 'element-ui'
+import { Button } from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 
-Vue.use(ElementUI)
+Vue.use(Button)
 
 Vue.config.productionTip = false
 
