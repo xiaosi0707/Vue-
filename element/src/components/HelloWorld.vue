b3ec99edf04f20492415cf607ac214b38a805c76
wyunfei <583514368@qq.com>
2018-01-24 10:04:50 +0800

    element button

--- element/src/components/HelloWorld.vue
+++ element/src/components/HelloWorld.vue
@@ -7,17 +7,17 @@
       <el-button type="warning">警告按钮</el-button>
       <el-button type="danger">危险按钮</el-button>
     </div>
-    <div class="block">
-      <span class="demonstration">默认不区分颜色{{ value1 }}</span>
-      <el-rate v-model="value1"></el-rate>
-    </div>
-    <div class="block">
-      <span class="demonstration">区分颜色</span>
-      <el-rate
-        v-model="value2"
-        :colors="['#99A9BF', '#F7BA2A', '#FF9900']">
-      </el-rate>
-    </div>
+    <!--<div class="block">-->
+      <!--<span class="demonstration">默认不区分颜色{{ value1 }}</span>-->
+      <!--<el-rate v-model="value1"></el-rate>-->
+    <!--</div>-->
+    <!--<div class="block">-->
+      <!--<span class="demonstration">区分颜色</span>-->
+      <!--<el-rate-->
+        <!--v-model="value2"-->
+        <!--:colors="['#99A9BF', '#F7BA2A', '#FF9900']">-->
+      <!--</el-rate>-->
+    <!--</div>-->
   </div>
 </template>
 
