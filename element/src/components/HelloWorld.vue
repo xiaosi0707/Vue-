3e720a151c2404c269b4ba56ffeb2bb033d1dd24
wyunfei <583514368@qq.com>
2018-01-24 15:01:25 +0800

    element 按需加载 main.js引入也可以了, Tabs

--- element/src/components/HelloWorld.vue
+++ element/src/components/HelloWorld.vue
@@ -1,45 +1,26 @@
 <template>
   <div class="hello">
-    <el-collapse v-model="activeNames" @change="handleChange">
-      <el-collapse-item title="一致性 Consistency" name="1">
-        <div>与现实生活一致：与现实生活的流程、逻辑保持一致，遵循用户习惯的语言和概念；</div>
-        <div>在界面中一致：所有的元素和结构需保持一致，比如：设计样式、图标和文本、元素的位置等。</div>
-      </el-collapse-item>
-      <el-collapse-item title="反馈 Feedback" name="2">
-        <div>控制反馈：通过界面样式和交互动效让用户可以清晰的感知自己的操作；</div>
-        <div>页面反馈：操作后，通过页面元素的变化清晰地展现当前状态。</div>
-      </el-collapse-item>
-      <el-collapse-item title="效率 Efficiency" name="3">
-        <div>简化流程：设计简洁直观的操作流程；</div>
-        <div>清晰明确：语言表达清晰且表意明确，让用户快速理解进而作出决策；</div>
-        <div>帮助用户识别：界面简单直白，让用户快速识别而非回忆，减少用户记忆负担。</div>
-      </el-collapse-item>
-      <el-collapse-item title="可控 Controllability" name="4">
-        <div>用户决策：根据场景可给予用户操作建议或安全提示，但不能代替用户进行决策；</div>
-        <div>结果可控：用户可以自由的进行操作，包括撤销、回退和终止当前操作等。</div>
-      </el-collapse-item>
-    </el-collapse>
+    <el-tabs v-model="activeName" @tab-click="handleClick">
+      <el-tab-pane label="用户管理" name="first">用户管理</el-tab-pane>
+      <el-tab-pane label="配置管理" name="second">配置管理</el-tab-pane>
+      <el-tab-pane label="角色管理" name="third">角色管理</el-tab-pane>
+      <el-tab-pane label="定时任务补偿" name="fourth">定时任务补偿</el-tab-pane>
+    </el-tabs>
   </div>
 </template>
 
 <script>
-import Vue from 'vue'
-import { Collapse, collapseItem } from 'element-ui'
-Vue.use(Collapse)
-Vue.use(collapseItem)
 export default {
   components: {
-    Collapse,
-    collapseItem
   },
   data () {
     return {
-      activeNames: ['1']
+      activeName: 'second'
     }
   },
   methods: {
-    handleChange (val) {
-      console.log(val)
+    handleClick (tab, event) {
+      console.log(tab, event)
     }
   }
 }
--- element/src/main.js
+++ element/src/main.js
@@ -3,7 +3,11 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import { Tabs, TabPane } from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+
+Vue.use(Tabs)
+Vue.use(TabPane)
 
 Vue.config.productionTip = false
 
