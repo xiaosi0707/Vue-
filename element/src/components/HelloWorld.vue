d27a682e2796fc201bcbd485048e54132ca8c13a
wyunfei <583514368@qq.com>
2018-01-24 10:01:12 +0800

    element-ui

---
+++ element/src/components/HelloWorld.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="hello">
+    <div>
+      <el-button type="primary">主要按钮</el-button>
+      <el-button type="success">成功按钮</el-button>
+      <el-button type="info">信息按钮</el-button>
+      <el-button type="warning">警告按钮</el-button>
+      <el-button type="danger">危险按钮</el-button>
+    </div>
+    <div class="block">
+      <span class="demonstration">默认不区分颜色{{ value1 }}</span>
+      <el-rate v-model="value1"></el-rate>
+    </div>
+    <div class="block">
+      <span class="demonstration">区分颜色</span>
+      <el-rate
+        v-model="value2"
+        :colors="['#99A9BF', '#F7BA2A', '#FF9900']">
+      </el-rate>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      value1: null,
+      value2: null
+    }
+  },
+  watch: {
+    value1 () {
+      console.log(this.value1)
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
