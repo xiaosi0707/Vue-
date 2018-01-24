813708aa741144dfedff82ae04e3b7eb8c89e2e6
wyunfei <583514368@qq.com>
2018-01-24 10:15:23 +0800

    element - loading

--- element/src/components/HelloWorld.vue
+++ element/src/components/HelloWorld.vue
@@ -23,6 +23,27 @@
       layout="prev, pager, next"
       :total="1000">
     </el-pagination>
+    <template>
+      <el-table
+        v-loading="loading"
+        :data="tableData"
+        style="width: 100%">
+        <el-table-column
+          prop="date"
+          label="日期"
+          width="180">
+        </el-table-column>
+        <el-table-column
+          prop="name"
+          label="姓名"
+          width="180">
+        </el-table-column>
+        <el-table-column
+          prop="address"
+          label="地址">
+        </el-table-column>
+      </el-table>
+    </template>
   </div>
 </template>
 
@@ -32,7 +53,21 @@ export default {
   data () {
     return {
       value1: null,
-      value2: null
+      value2: null,
+      tableData: [{
+        date: '2016-05-03',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄'
+      }, {
+        date: '2016-05-02',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄'
+      }, {
+        date: '2016-05-04',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄'
+      }],
+      loading: true
     }
   },
   watch: {
